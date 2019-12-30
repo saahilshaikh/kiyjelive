38f35d048cdc227f153a49087a4e5951e479d2f7
Flappify <creative.flappify@gmail.com>
2019-12-31 04:56:46 +0530

    Initial commit

---
+++ precache-manifest.de400ce42c9c051f232559744a21a68f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd247ee70a00653a9564ee268341f8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfe712f1935a276dbf3c",
+    "url": "/static/css/main.f818ea8d.chunk.css"
+  },
+  {
+    "revision": "a30fa13ac486606a75c9",
+    "url": "/static/js/2.f7955175.chunk.js"
+  },
+  {
+    "revision": "d855175fa88759f42ef775c426c7795a",
+    "url": "/static/js/2.f7955175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfe712f1935a276dbf3c",
+    "url": "/static/js/main.afc207ec.chunk.js"
+  },
+  {
+    "revision": "8d85d8f306643927082d",
+    "url": "/static/js/runtime-main.c3410e31.js"
+  },
+  {
+    "revision": "a41a6c816f406b74ac88bfd58bc46b33",
+    "url": "/static/media/404.a41a6c81.jpg"
+  },
+  {
+    "revision": "63c6450d6cced66b5e2d3781276dfdc0",
+    "url": "/static/media/login.63c6450d.jpg"
+  }
+]);
